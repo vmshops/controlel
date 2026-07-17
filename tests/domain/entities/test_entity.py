0017a77ecffec482180d991177c67a21ec951fd4
marsa99 <34799604+marsa99@users.noreply.github.com>
2026-07-17 10:33:16 +0200

    CT-0003 Add pre-commit developer workflow

--- tests/domain/entities/test_entity.py
+++ tests/domain/entities/test_entity.py
@@ -2,7 +2,6 @@ from controlel.domain.entities.entity import Entity
 
 
 def test_entity_creation():
-
     entity = Entity(name="Living Room", entity_type="zone")
 
     assert entity.name == "Living Room"
